48a2cdab754ef74158dc86db4134b39d7dd48cd2
Mathieu Deous <mdeous@atheos.fr>
2012-08-29 22:09:07 +0200

    create user listing script

---
+++ user-list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sqlite3
+import sys
+
+from config import DB_PATH
+
+
+if not os.path.exists(DB_PATH):
+    print "ERROR: Database not found: %s" % DB_PATH
+    sys.exit(2)
+
+db = sqlite3.connect(DB_PATH)
+cursor = db.cursor()
+
+cursor.execute("SELECT username FROM users;")
+users = cursor.fetchall()
+
+print "* OpenVPN access list:"
+for user in users:
+    print "  - %s" % user
